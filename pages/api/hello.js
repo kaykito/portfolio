2927bf3f8a183d166ee9639d496fffe998350883
Rizki Satriyo <rizki.sa7ria@gmail.com>
2025-04-06 18:59:41 +0700

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,9 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 export const runtime = 'edge';
 
-export default function handler(req) {
-  return new Response("Hello from Edge!");
-}
 export default function handler(req, res) {
   res.status(200).json({ name: 'John Doe' })
 }
